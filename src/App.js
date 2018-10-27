72296a1c907bfa9c9be592c159805aa4528d3d2e
benjamin-summers <summerstyler2@gmail.com>
2018-10-27 16:29:38 -0400

    creating flashcards for app
    creating components Card.jsx

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
+import Card from './Card/Card';
 
 export default class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            cards: [
+                {id: 1, eng: "English", han: "Hanzi", pin: "Pinyin"},
+                {id: 2, eng: "English", han: "Hanzi_2", pin: "Pinyin_2"},
+            ],
+            currentCard: {}
+        }
+    }
+
     render() {
         return (
             <div className="App">
-
+                <Card/>
             </div>
         )
     }
